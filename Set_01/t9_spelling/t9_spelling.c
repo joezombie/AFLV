9e4f3fd3981af00c7bc9c1941a14b6087945e881
Johannes Gunnar Heidarsson <joezombie@gmail.com>
2014-12-02 21:54:40 +0000

    t9

--- Set_01/t9_spelling/t9_spelling.c
+++ Set_01/t9_spelling/t9_spelling.c
@@ -21,9 +21,27 @@ int main(int argc, char** argv) {
     int nMessages;
     nMessages = (int) strtol(nMessagesStr, &ptr, 10);
 
-    printf("%d", nMessages);
-
     char messages[nMessages][messageMaxSize];
+    int messagesLen[nMessages];
 
+    for(int i = 0; i < nMessages; i++){
+        for(int j = 0; read(0, buf, sizeof(buf)) > 0; j++){
+            if(buf[0] == '\n'){
+                messages[i][j] = '\0';
+                messagesLen[i] = j;
+                break;
+            }else {
+                messages[i][j] = buf[0];
+            }
+        }
+    }
+
+    for(int i = 0; i < nMessages; i++){
+        for(int j = 0; j < messagesLen[i]; j++){
+            printf("%c", messages[i][j]);
+        }
+        printf("\n");
+    }
+    
     return (EXIT_SUCCESS);
 }
