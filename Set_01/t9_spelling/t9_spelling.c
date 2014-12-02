c1ea8839b9ff692b69f1385c5125c7723b000978
Johannes Gunnar Heidarsson <joezombie@gmail.com>
2014-12-02 21:12:14 +0000

    T9 spelling

--- Set_01/t9_spelling/t9_spelling.c
+++ Set_01/t9_spelling/t9_spelling.c
@@ -2,27 +2,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
-    const int inputMaxSize = 1000;
-	char buf[1];
-    char input[inputMaxSize];
-    int nInput;
-    int p1Changes = 0;
-    int p2Changes = 0;
-    int p3Changes = 0;
+    const int messageMaxSize = 1000;
+    char buf[1];
+    char nMessagesStr[messageMaxSize];
 
     for(int i = 0; read(0, buf, sizeof(buf)) > 0; i++){
         if(buf[0] == '\n'){
             break;
-        } else {
-            input[i] = buf[0];
-            nInput += 1;
+        }else {
+            nMessagesStr[i] = buf[0];
         }
     }
 
-    printf("%d\n%d\n%d\n", p1Changes, p2Changes, p3Changes);
+    char *ptr;
+    int nMessages;
+    nMessages = (int) strtol(nMessagesStr, &ptr, 10);
+
+    printf("%d", nMessages);
+
+    char messages[nMessages][messageMaxSize];
 
     return (EXIT_SUCCESS);
 }
-
