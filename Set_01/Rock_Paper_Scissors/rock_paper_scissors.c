24846326d1547d802eadc1266c12b0fa201dc5b1
Johannes Gunnar Heidarsson <joezombie@gmail.com>
2014-12-03 13:25:44 +0000

    T9 spelling

---
+++ Set_01/Rock_Paper_Scissors/rock_paper_scissors.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+  
+    return (EXIT_SUCCESS);
+}
--- Set_01/t9_spelling/t9_spelling.c
+++ Set_01/t9_spelling/t9_spelling.c
@@ -4,6 +4,108 @@
 #include <stdbool.h>
 #include <string.h>
 
+
+
+int getNrPresses(char c){
+    int result = 0;
+    switch(c){
+        case 'a':
+        case 'd':
+        case 'g':
+        case 'j':
+        case 'm':
+        case 'p':
+        case 't':
+        case 'w':
+        case ' ':
+            result = 1;
+            break;
+        case 'b':
+        case 'e':
+        case 'h':
+        case 'k':
+        case 'n':
+        case 'q':
+        case 'u':
+        case 'x':
+            result = 2;
+            break;
+        case 'c':
+        case 'f':
+        case 'i':
+        case 'l':
+        case 'o':
+        case 'r':
+        case 'v':
+        case 'y':
+            result = 3;
+            break;
+        case 's':
+        case 'z':
+            result = 4;
+            break;
+        default:
+            result = -1;
+            break;
+    }
+
+    return result;
+}
+
+int getKeyNr(char c){
+    int result = -1;
+
+    switch(c){
+        case 'a':
+        case 'b':
+        case 'c':
+            result = 2;
+            break;
+        case 'd':
+        case 'e':
+        case 'f':
+            result = 3;
+            break;
+        case 'g':
+        case 'h':
+        case 'i':
+            result = 4;
+            break;
+        case 'j':
+        case 'k':
+        case 'l':
+            result = 5;
+            break;
+        case 'm':
+        case 'n':
+        case 'o':
+            result = 6;
+            break;
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+            result = 7;
+            break;
+        case 't':
+        case 'u':
+        case 'v':
+            result = 8;
+            break;
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+            result = 9;
+            break;
+        case ' ':
+            result = 0;
+            break;
+    }
+
+    return result;
+}
+
 int main(int argc, char** argv) {
     const int messageMaxSize = 1000;
     char buf[1];
@@ -37,8 +139,15 @@ int main(int argc, char** argv) {
     }
 
     for(int i = 0; i < nMessages; i++){
+        printf("Case #%d: ", i + 1);
         for(int j = 0; j < messagesLen[i]; j++){
-            printf("%c", messages[i][j]);
+            int keyNr = getKeyNr(messages[i][j]);
+            for(int k = 0; k < getNrPresses(messages[i][j]); k++){
+                printf("%d", keyNr);
+            }
+            if(getKeyNr(messages[i][j]) == getKeyNr(messages[i][j+1])){
+                    printf(" ");
+            }
         }
         printf("\n");
     }
