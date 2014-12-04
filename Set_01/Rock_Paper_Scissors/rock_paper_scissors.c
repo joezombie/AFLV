ef495a11481af74ea37e1f8f70de4419d326245b
Johannes Gunnar Heidarsson <joezombie@gmail.com>
2014-12-04 19:33:02 +0000

    Meh

--- Set_01/Rock_Paper_Scissors/rock_paper_scissors.c
+++ Set_01/Rock_Paper_Scissors/rock_paper_scissors.c
@@ -4,7 +4,35 @@
 #include <stdbool.h>
 #include <string.h>
 
+char HANDS[3][9] = {
+    {"paper"},
+    {"rock"},
+    {"scissors"}
+};
+
 int main(int argc, char** argv) {
-  
+    int nPlayers = 0;
+    int nGames = 0;
+    while(scanf("%d %d\n", &nPlayers, &nGames) > 0){
+        if(nPlayers == 0){
+            break;
+        }
+        char moves[nGames][nPlayers][9];
+
+        for(int i = 0; i < nGames; i++){
+            for(int j = 0; j < nPlayers; j++){
+                int player = 0;
+                scanf("%d ", &player);            
+                scanf("%s", moves[i][player - 1]);
+            }
+        }
+
+        for(int i = 0; i < nGames; i++){
+            for(int j = 0; j < nPlayers; j++){
+                printf("%d %s ", j + 1, moves[i][j]);
+            }
+            printf("\n");
+        }
+    }
     return (EXIT_SUCCESS);
 }
