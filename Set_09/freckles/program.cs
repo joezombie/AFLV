42b5b283d3ddf3bfeb9e45daf30195774602bcb5
Johannes Gunnar Heidarsson <joezombie@gmail.com>
2014-12-14 01:51:09 +0000

    Freckles on CS

--- Set_09/freckles/program.cs
+++ Set_09/freckles/program.cs
@@ -29,21 +29,21 @@ public class Program {
     }
 
     public struct xy {
-        public xy(float x, float y){
+        public xy(double x, double y){
             this.x = x;
             this.y = y;
         }
         override public string ToString(){
             return x.ToString() + "," + y.ToString();
         }
-        public float x;
-        public float y;
+        public double x;
+        public double y;
     }
 
     public struct edge {
         public int u, v;
-        public float weight;
-        public edge(int _u, int _v, float _w) {
+        public double weight;
+        public edge(int _u, int _v, double _w) {
             u = _u;
             v = _v;
             weight = _w;
@@ -53,9 +53,9 @@ public class Program {
     public class EdgeComparer: IComparer<edge>{
         public int Compare(edge a, edge b){
             if(a.weight < b.weight){
-                return 1;
-            } else {
                 return -1;
+            } else {
+                return 1;
             }
         }
     }
@@ -90,41 +90,52 @@ public class Program {
 
         while(nCases-- > 0){
             int nFreckles = scanner.NextInt();
+            
+            /*
             adj = new List<edge>[nFreckles];
             for(int i = 0; i < nFreckles; i++){
                 adj[i] = new List<edge>();
-            }
-
+            }*/
+            List<edge> edges = new List<edge>();
             xy[] points = new xy[nFreckles];
 
         	for(int i = 0; i < nFreckles; i++){
                 points[i] = new xy(
-                    float.Parse(scanner.Next(), CultureInfo.InvariantCulture),
-                    float.Parse(scanner.Next(), CultureInfo.InvariantCulture)
+                    double.Parse(scanner.Next(), CultureInfo.InvariantCulture),
+                    double.Parse(scanner.Next(), CultureInfo.InvariantCulture)
                 );
         	}
 
             for(int i = 0; i < nFreckles; i++){
                 for(int j = i + 1; j < nFreckles; j++){
-                    float f = (float)Math.Sqrt(
+                    double f = (double)Math.Sqrt(
                         Math.Pow(points[i].x - points[j].x, 2) + 
                         Math.Pow(points[i].y - points[j].y, 2) 
                     );
-                    
+                    edges.Add(new edge(i, j, f));
+                    /*
                     adj[i].Add(new edge(i, j, f));
                     adj[j].Add(new edge(j, i, f));
+                    */
                 }
             }
 
-            float ans = float.MaxValue;
+            double ans = 0.0f;
+            foreach (edge e in mst(nFreckles, edges)) {
+                ans += e.weight;
+            }
+
+            /*
+            double ans = double.MaxValue;
             for(int i = 0; i < nFreckles; i++){
-                float cost = 0.0f;
+                double cost = 0.0f;
                 foreach (edge e in mst(nFreckles, adj[i])) {
                      cost += e.weight;
                 }
                 ans = Math.Min(ans, cost);
             }
-                writer.WriteLine(String.Format(CultureInfo.InvariantCulture, "{0:0.00}", ans));
+            */
+            writer.WriteLine(String.Format(CultureInfo.InvariantCulture, "{0:0.00}", ans));
         }
         
         writer.Flush();
