91d996bfa867acac6f9877b5714b586e1ab4b803
Johannes Gunnar Heidarsson <joezombie@gmail.com>
2014-12-15 11:02:39 +0000

    CPP Template

---
+++ Misc/cpp_template/program.cc
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <utility>
+#include <vector>
+#include <limits>
+#include <math.h>
+#include <iostream>       // std::cout
+#include <queue>          // std::priority_queue
+#include <functional>     // std::greater
+#include <algorithm>    // std::sort
+#include <iomanip>      // std::setprecision
+
+using namespace std; 
+
+int main(void) {
+    int nCases = 0;
+
+    while(nCases--){
+        int n;
+        scanf("%d\n", &n);     
+
+        printf("%d\n", n);
+    }
+
+
+    return 0;
+}
---
+++ Set_10/paintball/program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Kattis.IO;
+
+public class Program
+{
+    static public void Main ()
+    {
+        Scanner scanner = new Scanner();
+        BufferedStdoutWriter writer = new BufferedStdoutWriter();
+        
+        writer.WriteLine("Program");
+        
+        writer.Flush();
+    }
+}
